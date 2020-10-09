d97f45bce8588027f817599106ee7e1fba299c66
cogden <cogden1970@gmail.com>
2020-10-09 12:45:56 -0700

    Use more efficient calculation for finite differences.

--- src/transforms/transformations/FiniteDifferenceLinearization.h
+++ src/transforms/transformations/FiniteDifferenceLinearization.h
@@ -39,54 +39,58 @@ namespace OpenKalman
       static_assert((is_perturbation_v<Perturbations> and ...));
     }
 
-    template<std::size_t term, typename...Inputs>
-    auto jac_term(const std::tuple<Inputs...>& inputs) const
+    template<typename T>
+    static constexpr auto make_strict_typed_matrix(T&& t)
     {
-      using Scalar = typename MatrixTraits<decltype(std::get<term>(inputs))>::Scalar;
-      constexpr auto width = MatrixTraits<decltype(std::get<term>(inputs))>::dimension;
+      using RC = typename MatrixTraits<T>::RowCoefficients;
+      using CC = typename MatrixTraits<T>::ColumnCoefficients;
+      return TypedMatrix<RC, CC, strict_matrix_t<T>> {std::move(t)};
+    }
+
+    template<std::size_t term, typename...Inputs>
+    auto jac_term(std::tuple<Inputs...>&& inputs) const
+    {
+      using Traits = MatrixTraits<decltype(std::get<term>(inputs))>;
+      using Scalar = typename Traits::Scalar;
+      constexpr auto width = Traits::dimension;
       return apply_columnwise<width>([&](std::size_t i) {
         const Scalar h = std::get<term>(deltas)[i];
-        const auto t_start = internal::tuple_slice<0, term>(inputs);
-        const auto t_end = internal::tuple_slice<1, sizeof...(Inputs)>(inputs);
-
-        auto col = TypedMatrix {std::get<term>(inputs)};
+        auto& col = std::get<term>(inputs);
         const Scalar x = col[i];
         col[i] = x + h;
-        const auto xp = std::tuple_cat(t_start, std::tuple {col}, t_end);
+        const auto fp = std::apply(transformation, inputs);
         col[i] = x - h;
-        const auto xm = std::tuple_cat(t_start, std::tuple {col}, t_end);
-        return strict((std::apply(transformation, xp) - std::apply(transformation, xm))/(2*h));
+        const auto fm = std::apply(transformation, inputs);
+        col[i] = x;
+        return strict((fp - fm)/(2*h));
       });
     }
 
     template<typename...Inputs, std::size_t...terms>
-    auto jacobian_impl(const std::tuple<Inputs...>& inputs, std::index_sequence<terms...>) const
+    auto jacobian_impl(std::tuple<Inputs...>&& inputs, std::index_sequence<terms...>) const
     {
       static_assert(sizeof...(Inputs) == sizeof...(terms));
-      return std::tuple {jac_term<terms>(inputs) ...};
+      return std::tuple {jac_term<terms>(std::move(inputs)) ...};
     }
 
     template<std::size_t term, std::size_t i, std::size_t j, typename...Inputs>
-    auto h_j(const std::tuple<Inputs...>& inputs) const
+    auto h_j(std::tuple<Inputs...>&& inputs) const
     {
       using Scalar = typename MatrixTraits<decltype(std::get<term>(inputs))>::Scalar;
-      const auto t_start = internal::tuple_slice<0, term>(inputs);
-      const auto t_end = internal::tuple_slice<1, sizeof...(Inputs)>(inputs);
       const Scalar hi = std::get<term>(deltas)[i];
-      auto col = TypedMatrix {std::get<term>(inputs)};
+      auto& col = std::get<term>(inputs);
       const Scalar xi = col[i];
 
       if constexpr (i == j)
       {
-        const auto x0 = std::tuple_cat(t_start, std::tuple {col}, t_end);
-        const auto f0 = std::apply(transformation, x0);
+        const auto f0 = std::apply(transformation, inputs);
         col[i] = xi + hi;
-        const auto xp = std::tuple_cat(t_start, std::tuple {col}, t_end);
-        const auto fp = std::apply(transformation, xp);
+        const auto fp = std::apply(transformation, inputs);
         col[i] = xi - hi;
-        const auto xm = std::tuple_cat(t_start, std::tuple {col}, t_end);
-        const auto fm = std::apply(transformation, xm);
-        return strict((fp - 2*f0 + fm) / (hi*hi));
+        const auto fm = std::apply(transformation, inputs);
+        col[i] = xi;
+        auto ret = strict((fp - 2*f0 + fm) / (hi * hi));
+        return ret;
       }
       else
       {
@@ -94,52 +98,53 @@ namespace OpenKalman
         const Scalar xj = col[j];
         col[i] = xi + hi;
         col[j] = xj + hj;
-        const auto xpp = std::tuple_cat(t_start, std::tuple {col}, t_end);
+        const auto fpp = std::apply(transformation, inputs);
         col[j] = xj - hj;
-        const auto xpm = std::tuple_cat(t_start, std::tuple {col}, t_end);
+        const auto fpm = std::apply(transformation, inputs);
         col[i] = xi - hi;
-        const auto xmm = std::tuple_cat(t_start, std::tuple {col}, t_end);
+        const auto fmm = std::apply(transformation, inputs);
         col[j] = xj + hj;
-        const auto xmp = std::tuple_cat(t_start, std::tuple {col}, t_end);
-        const auto fp = (std::apply(transformation, xpp) - std::apply(transformation, xpm)) / (2 * hj);
-        const auto fm = (std::apply(transformation, xmp) - std::apply(transformation, xmm)) / (2 * hj);
-        return strict((fp - fm) / (2 * hi));
+        const auto fmp = std::apply(transformation, inputs);
+        col[i] = xi;
+        col[j] = xj;
+        auto ret = strict((fpp - fpm - fmp + fmm) / (4 * hi * hj));
+        return ret;
       }
     };
 
     template<std::size_t term, std::size_t i, typename...Inputs, std::size_t...js>
-    auto h_i(const std::tuple<Inputs...>& inputs, std::index_sequence<js...>) const
+    auto h_i(std::tuple<Inputs...>&& inputs, std::index_sequence<js...>) const
     {
-      return std::array {TypedMatrix {h_j<term, i, js>(inputs)}...};
+      return std::array {h_j<term, i, js>(std::move(inputs))...};
     }
 
     template<std::size_t term, typename...Inputs, std::size_t...is>
-    auto h_k(const std::tuple<Inputs...>& inputs, std::index_sequence<is...>) const
+    auto h_k(std::tuple<Inputs...>&& inputs, std::index_sequence<is...>) const
     {
       constexpr auto j_size = MatrixTraits<decltype(std::get<term>(inputs))>::dimension;
-      using A = decltype(h_i<term, 0>(inputs, std::make_index_sequence<j_size>()));
-      return std::array<A, sizeof...(is)> {h_i<term, is>(inputs, std::make_index_sequence<j_size>())...};
+      using A = decltype(h_i<term, 0>(std::move(inputs), std::make_index_sequence<j_size>()));
+      return std::array<A, sizeof...(is)> {h_i<term, is>(std::move(inputs), std::make_index_sequence<j_size>())...};
     }
 
     template<std::size_t term, typename...Inputs, std::size_t...ks>
-    auto h_term(const std::tuple<Inputs...>& inputs, std::index_sequence<ks...>) const
+    auto h_term(std::tuple<Inputs...>&& inputs, std::index_sequence<ks...>) const
     {
-      constexpr auto i_size = MatrixTraits<decltype(std::get<term>(inputs))>::dimension;
-      auto t = h_k<term>(inputs, std::make_index_sequence<i_size>());
-      constexpr auto width = MatrixTraits<decltype(std::get<term>(inputs))>::dimension;
-      using Scalar = typename MatrixTraits<decltype(std::get<term>(inputs))>::Scalar;
-      using C = typename MatrixTraits<decltype(std::get<term>(inputs))>::RowCoefficients;
-      using Vb = typename MatrixTraits<decltype(std::get<term>(inputs))>::template StrictMatrix<width, width>;
+      using TermTrait = MatrixTraits<decltype(std::get<term>(inputs))>;
+      constexpr auto i_size = TermTrait::dimension;
+      const auto t = h_k<term>(std::move(inputs), std::make_index_sequence<i_size>());
+      constexpr auto width = TermTrait::dimension;
+      using C = typename TermTrait::RowCoefficients;
+      using Vb = typename TermTrait::template StrictMatrix<width, width>;
       using V = TypedMatrix<C, C, Vb>;
-      return std::array {apply_coefficientwise<V>([&](std::size_t i, std::size_t j) { return Scalar(t[i][j][ks]); })...};
+      return std::array {apply_coefficientwise<V>([&](std::size_t i, std::size_t j) { return t[i][j][ks]; })...};
     }
 
     template<typename...Inputs, std::size_t...terms>
-    auto hessian_impl(const std::tuple<Inputs...>& inputs, std::index_sequence<terms...>) const
+    auto hessian_impl(std::tuple<Inputs...>&& inputs, std::index_sequence<terms...>) const
     {
       static_assert(sizeof...(Inputs) == sizeof...(terms));
-      constexpr auto k_size = MatrixTraits<std::invoke_result_t<Trans, std::decay_t<InDelta>, std::decay_t<PsDelta>...>>::dimension;
-      return std::tuple {h_term<terms>(inputs, std::make_index_sequence<k_size>())...};
+      constexpr auto k_size = MatrixTraits<std::invoke_result_t<Trans, Inputs...>>::dimension;
+      return std::tuple {h_term<terms>(std::move(inputs), std::make_index_sequence<k_size>())...};
     }
 
   public:
@@ -148,23 +153,29 @@ namespace OpenKalman
     auto operator()(In&& in, Perturbations&& ... ps) const
     {
       check_inputs(in, ps...);
-      return transformation(in, ps...);
+      return transformation(std::forward<In>(in), std::forward<Perturbations>(ps)...);
     }
 
     /// Returns a tuple of the Jacobians for the input and each perturbation term.
     template<typename In, typename ... Perturbations>
-    auto jacobian(const In& in, const Perturbations&...ps) const
+    auto jacobian(In&& in, Perturbations&&...ps) const
     {
       check_inputs(in, ps...);
-      return jacobian_impl(std::tuple {in, ps...}, std::make_index_sequence<1 + sizeof...(ps)>());
+      return jacobian_impl(
+        std::forward_as_tuple(make_strict_typed_matrix(std::forward<In>(in)),
+          make_strict_typed_matrix(std::forward<Perturbations>(ps))...),
+        std::make_index_sequence<1 + sizeof...(ps)>());
     }
 
     /// Returns a tuple of Hessian matrices for the input and each perturbation term.
     template<typename In, typename ... Perturbations>
-    auto hessian(const In& in, const Perturbations&...ps) const
+    auto hessian(In&& in, Perturbations&&...ps) const
     {
       check_inputs(in, ps...);
-      return hessian_impl(std::tuple {in, ps...}, std::make_index_sequence<1 + sizeof...(ps)>());
+      return hessian_impl(
+        std::forward_as_tuple(make_strict_typed_matrix(std::forward<In>(in)),
+          make_strict_typed_matrix(std::forward<Perturbations>(ps))...),
+        std::make_index_sequence<1 + sizeof...(ps)>());
     }
 
   protected:
